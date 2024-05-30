f342b3344d02ac12571bdea1af02aaa4acf59aca
kavinkumar.ct21@bitsathy.ac.in <kavinkumar.ct21@bitsathy.ac.in>
2024-05-30 21:54:03 +0530

    added projects page and configure with email js

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,7 +2,8 @@ import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import Navbar from './Navbar';
+import { FiLinkedin,FiGithub, FiMail } from 'react-icons/fi';
 const Contact = () => {
   const form = useRef();
 
@@ -22,16 +23,19 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-        <h2 className="mb-4 text-2xl font-bold text-center text-gray-800">Contact Us</h2>
+    <div className='bg-gradient-to-b from-[#e2e7f6]'>
+    <Navbar />
+    
+    <div className="mx-4 flex flex-col items-center md:flex-row min-h-screen justify-between md:mx-20 mb-10">
+      <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-lg md:mt-28">
+        <h2 className="mb-4 text-2xl font-bold text-center text-gray-800">Message me</h2>
         <form ref={form} onSubmit={sendEmail} className="space-y-4">
           <div>
             <label className="block mb-2 text-sm font-bold text-gray-700">Name</label>
             <input
               type="text"
               name="from_name"
-              className="w-full px-3 py-2 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-3 py-2 text-sm  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 border-2 border-gray-200 "
               required
             />
           </div>
@@ -40,7 +44,7 @@ const Contact = () => {
             <input
               type="email"
               name="from_email"
-              className="w-full px-3 py-2 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-3 py-2 text-sm border-2 border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               required
             />
           </div>
@@ -48,14 +52,14 @@ const Contact = () => {
             <label className="block mb-2 text-sm font-bold text-gray-700">Message</label>
             <textarea
               name="message"
-              className="w-full h-32 px-3 py-2 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full h-20 px-3 py-2 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 border-2 border-gray-200 "
               required
             ></textarea>
           </div>
           <div>
             <button
               type="submit"
-              className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+              className="w-full px-4 py-2 font-bold text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
             >
               Send
             </button>
@@ -63,6 +67,51 @@ const Contact = () => {
         </form>
       </div>
       <ToastContainer />
+      
+      <div className="w-full max-w-md p-8 rounded-lg mt-10 md:mt-20">
+
+        <div className=' max-w-md p-8 space-y-20 w-3/2'>
+          <div className='flex flex-col'>
+            <div className='bg-white shadow-md flex flex-row space-x-10 p-4 mb-4 w-full'>
+            <div>
+              <FiLinkedin />
+            </div>
+            <h1>Linked IN</h1>
+            </div>
+
+            <div className='bg-white shadow-md flex flex-row space-x-10 p-4 mb-4'>
+            <div>
+              <FiLinkedin />
+            </div>
+            <h1>Linked IN</h1>
+            </div>
+
+            <div className='bg-white shadow-md flex flex-row space-x-10 p-4 mb-4'>
+            <div>
+              <FiLinkedin />
+            </div>
+            <h1>Linked IN</h1>
+            </div>
+
+            <div className='bg-white shadow-md flex flex-row space-x-10 p-4 mb-4'>
+            <div>
+              <FiLinkedin />
+            </div>
+            <h1>Linked IN</h1>
+            </div>
+
+
+            <div className='bg-white shadow-md flex flex-row space-x-10 p-4 mb-4'>
+            <div>
+              <FiLinkedin />
+            </div>
+            <h1>Linked IN</h1>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
